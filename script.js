3bbcb0503e0be079fe75ab4d53dd7a400aec4767
Surafels <ssurafel4@gmail.com>
2023-08-15 23:27:41 +0300

    Add local storage and function for remove button

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const bookTitle=document.getElementById('bookTitle');
 const bookAuthor=document.getElementById('bookAuthor');
 const addBtn=document.getElementById('btn');
+const listItem=document.getElementById('listItem');
 
 addBtn.onclick=function(){
     const title=bookTitle.value;
     const author=bookAuthor.value;
-console.log(title);
-console.log(author);
+//console.log(title);
+//console.log(author);
 if (title && author){
     localStorage.setItem(title, author);
     location.reload();
 }
 };
-for(let i=0; i<localStorage.length;i++){
-    
+for (let i = 0; i < localStorage.length; i++) {
+    const  title=localStorage.key(i);
+    const author=localStorage.getItem(title);
+    listItem.innerHTML+= `
+    ${title}: ${author}
+     <button class="remBtn" data-title="${title}">remove</button><br>`;
 }
+  const removeButtons=document.getElementsByClassName('remBtn');
+  
+  for (let i=0; i < removeButtons.length; i++){
+    removeButtons[i].addEventListener('click', function() {
+       const title = this.dataset.title;
+       localStorage.removeItem(title);
+       location.reload();
+    });
+  }
