c0dea0da86b8d7e6e66b7a04f2db892c9315a237
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-16 16:59:52 +0300

    Capture values for title and author

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const bookAuthor = document.getElementById('bookAuthor');
 const addBtn = document.getElementById('btn');
 const listItem = document.getElementById('listItem');
 
-addBtn.onclick = function () {
-  const title = bookTitle.value;
-  const author = bookAuthor.value;
-  if (title && author) {
-    localStorage.setItem(title, author);
-    location.reload();
-  }
+const books = [];
+
+addBtn.onclick = () => {
+  const title = bookTitle.value.trim();
+  const author = bookAuthor.value.trim();
 };
+
 for (let i = 0; i < localStorage.length; i++) {
   const title = localStorage.key(i);
   const author = localStorage.getItem(title);
   listItem.innerHTML += `
-    ${title}: ${author}
-     <button class="remBtn" data-title="${title}">remove</button><br>`;
+    ${title}: ${author}                                                 
+     <button class="remBtn" data-title="${title}">Remove</button><br>`;
 }
+
 const removeButtons = document.getElementsByClassName('remBtn');
 
 for (let i = 0; i < removeButtons.length; i++) {
