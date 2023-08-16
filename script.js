c6cf14b019a02f671fc11bdf927d590709dd70e9
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-16 18:58:58 +0300

    Refactor functionalities using functions

--- script.js
+++ script.js
@@ -5,38 +5,47 @@ const listEntry = document.getElementById('list-entry');
 
 const books = JSON.parse(localStorage.getItem('books')) || [];
 
-addBtn.addEventListener('click', () => {
-  const title = bookTitle.value.trim();
-  const author = bookAuthor.value.trim();
+const storeEntry = () => {
+  addBtn.addEventListener('click', () => {
+    const title = bookTitle.value.trim();
+    const author = bookAuthor.value.trim();
 
-  if (title.length > 0 && author.length > 0) {
-    const book = { title, author };
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
+    if (title.length > 0 && author.length > 0) {
+      const book = { title, author };
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
 
-    title.value = '';
-    title.author = '';
-  }
+      title.value = '';
+      title.author = '';
+    }
 
-  location.reload();
-});
+    location.reload();
+  });
+};
+storeEntry();
 
-books.map((book, index) => {
-  listEntry.innerHTML += `
+const showBookList = () => {
+  books.map((book, index) => {
+    listEntry.innerHTML += `
     <p>${book.title}</p>
     <p>${book.author}</p>
     <button class="rem-btn" data-index="${index}">Remove</button>
     <hr>
   `;
-});
+  });
+};
+showBookList();
 
 const removeButtons = document.getElementsByClassName('rem-btn');
 
-Array.from(removeButtons).map((btn, i) => {
-  btn.addEventListener('click', function () {
-    const index = parseInt(this.dataset.index);
-    books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(books));
-    location.reload();
+const removeBook = () => {
+  Array.from(removeButtons).map((btn, i) => {
+    btn.addEventListener('click', function () {
+      const index = parseInt(this.dataset.index);
+      books.splice(index, 1);
+      localStorage.setItem('books', JSON.stringify(books));
+      location.reload();
+    });
   });
-});
+};
+removeBook();
