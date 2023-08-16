3d811d8f4cd39b07689e32467b576f6ad2f6a6fe
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-16 15:47:09 +0300

    Reorder markup

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const bookTitle=document.getElementById('bookTitle');
-const bookAuthor=document.getElementById('bookAuthor');
-const addBtn=document.getElementById('btn');
-const listItem=document.getElementById('listItem');
+const bookTitle = document.getElementById('bookTitle');
+const bookAuthor = document.getElementById('bookAuthor');
+const addBtn = document.getElementById('btn');
+const listItem = document.getElementById('listItem');
 
-addBtn.onclick=function(){
-    const title=bookTitle.value;
-    const author=bookAuthor.value;
-if (title && author){
+addBtn.onclick = function () {
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  if (title && author) {
     localStorage.setItem(title, author);
     location.reload();
-}
+  }
 };
 for (let i = 0; i < localStorage.length; i++) {
-    const  title=localStorage.key(i);
-    const author=localStorage.getItem(title);
-    listItem.innerHTML+= `
+  const title = localStorage.key(i);
+  const author = localStorage.getItem(title);
+  listItem.innerHTML += `
     ${title}: ${author}
      <button class="remBtn" data-title="${title}">remove</button><br>`;
 }
-  const removeButtons=document.getElementsByClassName('remBtn');
-  
-  for (let i=0; i < removeButtons.length; i++){
-    removeButtons[i].addEventListener('click', function() {
-       const title = this.dataset.title;
-       localStorage.removeItem(title);
-       location.reload();
-    });
-  }
+const removeButtons = document.getElementsByClassName('remBtn');
+
+for (let i = 0; i < removeButtons.length; i++) {
+  removeButtons[i].addEventListener('click', function () {
+    const title = this.dataset.title;
+    localStorage.removeItem(title);
+    location.reload();
+  });
+}
