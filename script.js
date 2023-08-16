1821f740786868758aaf58e4a4565305b085e409
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-16 17:39:29 +0300

    Trim leading and trailing whitespace s in inputs

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-const bookTitle=document.getElementById('bookTitle');
-const bookAuthor=document.getElementById('bookAuthor');
-const addBtn=document.getElementById('btn');
-const listItem=document.getElementById('listItem');
+const bookTitle = document.getElementById('bookTitle');
+const bookAuthor = document.getElementById('bookAuthor');
+const addBtn = document.getElementById('btn');
+const listItem = document.getElementById('listItem');
 
-const books= JSON.parse(localStorage.getItem('books')) || [];
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
+addBtn.onclick = function () {
+  const title = bookTitle.value.trim();
+  const author = bookAuthor.value.trim();
 
-addBtn.onclick=function(){
-    const title=bookTitle.value;
-    const author=bookAuthor.value;
-
-
-if (title && author){
-
-  
-  const book= {title, author};
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  location.reload();
-
-}
+  if (title && author) {
+    const book = { title, author };
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+    location.reload();
+  }
 };
 
-for (let i=0;i<books.length;i++) {
+for (let i = 0; i < books.length; i++) {
   const book = books[i];
   listItem.innerHTML += `
   ${book.title}:${book.author}
   <button class="remBtn" data-index="${i}">remove</button><br>
-  `
-
+  `;
 }
-  const removeButtons=document.getElementsByClassName('remBtn');
-  
-  for (let i=0; i < removeButtons.length; i++){
-    removeButtons[i].addEventListener('click', function() {
-      const index=parseInt(this.dataset.index);
-      books.splice(index, 1);
-       localStorage.setItem('books',JSON.stringify(books));
-       location.reload();
-    });
-  }
+const removeButtons = document.getElementsByClassName('remBtn');
+
+for (let i = 0; i < removeButtons.length; i++) {
+  removeButtons[i].addEventListener('click', function () {
+    const index = parseInt(this.dataset.index);
+    books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+    location.reload();
+  });
+}
