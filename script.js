095e01b91ba5b2282c66d7a9b87bd4383149ab78
Surafels <ssurafel4@gmail.com>
2023-08-17 17:10:55 +0300

    Refract the code

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-const bookTitle = document.getElementById('book-title');
+const bookTitle= document.getElementById('book-title');
 const bookAuthor = document.getElementById('book-author');
 const addBtn = document.getElementById('btn');
 const listEntry = document.getElementById('list-entry');
 
+
+class Book{
+  constructor(title, author){
+    this.title = title;
+    this.author = author;
+  }
+static addBook(){
+   const  title = bookTitle.value.trim();
+     const author = bookAuthor.value.trim();
+    if (title.length > 0 && author.length > 0) {
+      const newBook = new Book(title, author );
+      const book={title,author}
+      
+      const bookCollection =document.createElement( 'li' );
+      bookCollection.classList.add( 'book-card' );
+
+      bookCollection.innerHTML += `
+        
+            <li class="book-card">
+              <p class="book-title">${book.title}</p>
+              <p class="book-title">${book.author}</p>
+              <button class="remove-btn">Remove</button>
+            </li>
+            <hr />`;
+        listEntry.appendChild(bookCollection);
+      title.value = '';
+      title.author = '';
+   
+  console.log(`Added book: ${newBook.title} by ${newBook.author}`)
+}
+
+}
+}
+
+addBtn.addEventListener('click',Book.addBook);
+
+
+
+/*
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
 const addBook = () => {
@@ -52,3 +91,4 @@ removeBtns.forEach((btn) => {
     this.location.reload();
   });
 });
+*/
