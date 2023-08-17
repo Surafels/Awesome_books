b38aae6e8c054677c23bf01b7db71e94b0a89957
FilbertKimasa <filbertdan67@gmail.com>
2023-08-17 09:28:57 +0300

    Modify script.js file

--- script.js
+++ script.js
@@ -2,50 +2,55 @@ const bookTitle = document.getElementById('book-title');
 const bookAuthor = document.getElementById('book-author');
 const addBtn = document.getElementById('btn');
 const listEntry = document.getElementById('list-entry');
-
-const books = JSON.parse(localStorage.getItem('books')) || [];
-
-const storeEntry = () => {
-  addBtn.addEventListener('click', () => {
-    const title = bookTitle.value.trim();
-    const author = bookAuthor.value.trim();
-
-    if (title.length > 0 && author.length > 0) {
-      const book = { title, author };
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
-
-      title.value = '';
-      title.author = '';
-    }
-
-    location.reload();
-  });
+let books = JSON.parse(localStorage.getItem('books')) || [];
+const addBook = () => {
+  const title = bookTitle.value.trim();
+  const author = bookAuthor.value.trim();
+  if (title.length > 0 && author.length > 0) {
+    const book = { title, author };
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+    title.value = '';
+    title.author = '';
+  }
+  this.location.reload();
 };
-storeEntry();
 
-const showBookList = () => {
-  books.map((book, index) => {
-    listEntry.innerHTML += `
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <button class="rem-btn" data-index="${index}">Remove</button>
-    <hr>
-  `;
-  });
+const updateStorage = () => {
+  localStorage.setItem('books', JSON.stringify(books));
 };
-showBookList();
 
-const removeButtons = document.getElementsByClassName('rem-btn');
-
-const removeBook = () => {
-  Array.from(removeButtons).map((btn) => {
-    btn.addEventListener('click', function () {
-      const index = parseInt(this.dataset.index);
-      books.splice(index, 1);
-      localStorage.setItem('books', JSON.stringify(books));
-      location.reload();
-    });
-  });
+const removeBook = (title, author) => {
+  books = books.filter(
+    (book) => book.title !== title && book.author !== author
+  );
+  updateStorage();
 };
-removeBook();
+
+addBtn.addEventListener('click', addBook);
+
+listEntry.innerHTML = `${books
+  .map(
+    (book) => `<div class="book-card">
+          <p class="book-title">
+            ${book.title}
+          </p>
+          <p class="book-title">
+           ${book.author}
+          </p>
+          <button class="remove-btn">Remove</button>
+        </div>
+        <hr />`
+  )
+  .join('')}`;
+
+const removeBtns = document.querySelectorAll('.remove-btn');
+removeBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const card = e.target.closest('.book-card');
+    const title = card.querySelector('.book-title');
+    const author = card.querySelector('.book-author');
+    removeBook(title.innerText, author);
+    this.location.reload();
+  });
+});
