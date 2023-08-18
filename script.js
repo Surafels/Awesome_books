395722b2ee63b4a85071d324d5f2b68b1e7f7ae6
Surafels <ssurafel4@gmail.com>
2023-08-18 11:13:15 +0300

    Fix errors

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ class Book {
     if (title.length > 0 && author.length > 0) {
       const book = { title, author };
       const same = this.books.some(
-        bk => JSON.stringify(bk) === JSON.stringify(book)
+        (bk) => JSON.stringify(bk) === JSON.stringify(book),
       );
       if (!same) {
         this.books.push(book);
@@ -36,7 +36,7 @@ class Book {
 
   removeBook(title, author) {
     this.books = this.books.filter(
-      book => book.title !== title && book.author !== author
+      (book) => book.title !== title && book.author !== author,
     );
     this.updateStorage();
     this.renderBooks();
@@ -50,7 +50,7 @@ class Book {
   renderBooks() {
     this.listEntry.innerHTML = this.books
       .map(
-        book => `
+        (book) => `
           <li class="book-card">
             <p class="entry">
               <span class="book-title">"${book.title}"</span> by
@@ -58,14 +58,14 @@ class Book {
             </p>
             <button class="remove-btn">Remove</button>
           </li>
-        `
+        `,
       )
       .join('');
 
     const removeBtns = document.querySelectorAll('.remove-btn');
 
-    removeBtns.forEach(btn => {
-      btn.addEventListener('click', e => {
+    removeBtns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
         const card = e.target.closest('.book-card');
         const title = card.querySelector('.book-title').innerText;
         const author = card.querySelector('.book-author').innerText;
